2295881cb52b5f692b4a147efb2504600b091064
2a6m <maxime.bourguignon1996@gmail.com>
2019-08-31 19:40:04 +0200

    modify to work on debian and with 2 batteries

--- cmd/battery/main.go
+++ cmd/battery/main.go
@@ -4,10 +4,67 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 
 	"io/ioutil"
+	"path/filepath"
 )
+
+const sysfs = "/sys/class/power_supply"
+
+func readFloat(path, filename string) (float64, error) {
+	str, err := ioutil.ReadFile(filepath.Join(path, filename))
+	if err != nil {
+		return 0, err
+	}
+	num, err := strconv.ParseFloat(string(str[:len(str)-1]), 64)
+	if err != nil {
+		return 0, err
+	}
+	return num, nil
+}
+
+func isBattery(path string) bool {
+	t, err := ioutil.ReadFile(filepath.Join(path,"type"))
+	return err == nil && string(t) == "Battery\n"
+}
+
+func getBatteryFiles() ([]string, error) {
+	files, err := ioutil.ReadDir(sysfs)
+	if err != nil {
+		return nil, err
+	}
+
+	var bFiles []string
+	for _, file := range files {
+		//fmt.Println(file.Name())
+		path := filepath.Join(sysfs, file.Name())
+		if isBattery(path) {
+			bFiles = append(bFiles, path)
+		}
+	}
+	return bFiles, err
+}
+
+func battery() (float64, error) {
+	batteryFiles, err := getBatteryFiles()
+	if err != nil {
+		return 0, err
+	}
+	var charge float64
+	var total float64
+	for _, file := range batteryFiles {
+		c, err := readFloat(file, "energy_now")
+		t, err := readFloat(file, "energy_full_design")
+		if err != nil {
+			return 0, err
+		}
+		charge += c
+		total += t
+	}
+	capacity := (charge / total)*100
+	return capacity, nil
+}
+
 
 func main() {
 
@@ -15,94 +72,26 @@ func main() {
 	var output string
 	var fullText string = "error"
 	var shortText string = "error"
+	var color string = "#ff0000"
 
 	// Read charging status information from kernel
 	// pseudo-file-system mounted at /sys.
-	chargingRaw, err := ioutil.ReadFile("/sys/class/power_supply/BAT0/status")
+	b, err := battery()
 	if err != nil {
-
-		// Write an error to STDERR, fallback display values
-		// to STDOUT and exit with failure code.
-		fmt.Fprintf(os.Stderr, "[i3blocks-go] Failed to read charging file: %s", err.Error())
-		fmt.Fprintf(os.Stdout, "%s\n%s\n", fullText, shortText)
+		fmt.Fprintf(os.Stderr, "[i3blocks-go] Failed to read battery files: %s", err.Error())
+		fmt.Fprintf(os.Stdout, "%s\n%s\n%s", fullText, shortText, color)
 		os.Exit(0)
 	}
 
-	// Read full capacity information from /sys.
-	chargeFullRaw, err := ioutil.ReadFile("/sys/class/power_supply/BAT0/charge_full")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "[i3blocks-go] Failed to read full capacity file: %s", err.Error())
-		fmt.Fprintf(os.Stdout, "%s\n%s\n", fullText, shortText)
-		os.Exit(0)
-	}
-
-	// Read current capacity information from /sys.
-	chargeNowRaw, err := ioutil.ReadFile("/sys/class/power_supply/BAT0/charge_now")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "[i3blocks-go] Failed to read current capacity file: %s", err.Error())
-		fmt.Fprintf(os.Stdout, "%s\n%s\n", fullText, shortText)
-		os.Exit(0)
-	}
-
-	// Trim whitespace.
-	charging := strings.TrimSpace(string(chargingRaw))
-	chargeFullString := strings.TrimSpace(string(chargeFullRaw))
-	chargeNowString := strings.TrimSpace(string(chargeNowRaw))
-
-	// Convert full capacity string to float32.
-	chargeFull, err := strconv.ParseFloat(chargeFullString, 32)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "[i3blocks-go] Could not convert full capacity value: %s", err.Error())
-		fmt.Fprintf(os.Stdout, "%s\n%s\n", fullText, shortText)
-		os.Exit(0)
-	}
-
-	// Convert current capacity string to float32.
-	chargeNow, err := strconv.ParseFloat(chargeNowString, 32)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "[i3blocks-go] Could not convert current capacity value: %s", err.Error())
-		fmt.Fprintf(os.Stdout, "%s\n%s\n", fullText, shortText)
-		os.Exit(0)
-	}
-
-	// Calculate current battery charge percentage.
-	chargePerc := int((chargeNow / chargeFull) * 100.0)
-
-	// Avoid unreasonable rounding or reporting errors.
-	if chargePerc > 100 {
-		chargePerc = 100
-	}
-
-	if (chargePerc < 8) && (charging != "Charging") {
-
-		// If charge percentage is very low and battery
-		// is currently not being charged, print values
-		// and exit with urgent return code.
-		fmt.Fprintf(os.Stdout, "%s\n%s\n", fullText, shortText)
-		os.Exit(33)
-	}
-
-	// Construct and color final output string based
-	// on charging status and percentage.
-	if charging == "Charging" {
-		output = fmt.Sprintf("<span foreground=\"#378c1a\">%d%%</span>", chargePerc)
+	output = fmt.Sprintf("bat: %.2f%%", b)
+	if b < 20 {
+		// if notification flag
 	} else {
-
-		if (chargePerc >= 0) && (chargePerc <= 20) {
-			output = fmt.Sprintf("<span foreground=\"#ff0000\">%d%%</span>", chargePerc)
-		} else if (chargePerc >= 21) && (chargePerc <= 30) {
-			output = fmt.Sprintf("<span foreground=\"#ffae00\">%d%%</span>", chargePerc)
-		} else if (chargePerc >= 31) && (chargePerc <= 40) {
-			output = fmt.Sprintf("<span foreground=\"#fff600\">%d%%</span>", chargePerc)
-		} else {
-			output = fmt.Sprintf("%d%%", chargePerc)
-		}
+		color = "#000000"
 	}
-
 	fullText = output
 	shortText = output
 
-	// Write out gathered information to STDOUT.
-	fmt.Fprintf(os.Stdout, "%s\n%s\n", fullText, shortText)
+	fmt.Fprintf(os.Stdout, "%s\n%s\n%s", fullText, shortText, color)
 	os.Exit(0)
 }
