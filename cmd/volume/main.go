ecad4f291787f41bed8e524cec363ac49052496e
2a6m <maxime.bourguignon1996@gmail.com>
2019-08-31 20:17:16 +0200

    home sweet home mod

--- cmd/volume/main.go
+++ cmd/volume/main.go
@@ -16,15 +16,13 @@ import (
 // and attempts to extract volume level
 // as well as muted information from the
 // commands output.
-func volume() (int, error) {
+func volume() (string, int, error) {
 
 	// Define aregular expression matching for
 	// currently set volume level.
-	var volRegex = regexp.MustCompile(`\[(\d{1,3})\%\]`)
-
-	// Return value for volume level. Setting it
-	// to -1 indicates that speakers are muted.
-	vol := 0
+	var volRegex = regexp.MustCompile(`\[(\d{1,3})\%\].*\[(\S*)dB\].*\[(\S*)\]`)
+	var status = ""
+	var vol = -1
 
 	// Execute 'amixer sget Master' and store
 	// output of this command written to either
@@ -32,7 +30,7 @@ func volume() (int, error) {
 	cmd := exec.Command("amixer", "sget", "Master")
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		return 0, err
+		return "", 0, err
 	}
 
 	scanner := bufio.NewScanner(bytes.NewBuffer(out))
@@ -45,29 +43,25 @@ func volume() (int, error) {
 		// for speaker sub-string.
 		if strings.Contains(text, "Mono: Playback") {
 
-			// If speakers are muted, return -1.
-			if strings.Contains(text, "[off]") {
-				return -1, nil
-			}
-
 			// Otherwise, attempt to match above regex
 			// on speaker string.
 			matches := volRegex.FindStringSubmatch(text)
-			if len(matches) != 2 {
-				return 0, fmt.Errorf("expected two matches but found %d", len(matches))
+			if len(matches) != 4 {
+				return "", 0, fmt.Errorf("expected two matches but found %d", len(matches))
 			}
 
 			// Convert extracted volume string to integer.
 			vol, err = strconv.Atoi(matches[1])
+			status = matches[3]
 			if err != nil {
-				return 0, err
+				return "", 0, err
 			}
 
 			break
 		}
 	}
 
-	return vol, nil
+	return status, vol, nil
 }
 
 func main() {
@@ -76,9 +70,10 @@ func main() {
 	var output string
 	var fullText string = "unknown"
 	var shortText string = "unknown"
+	var color string = "#ff0000"
 
 	// Retrieve current volume status from amixer.
-	vol, err := volume()
+	status, vol, err := volume()
 	if err != nil {
 
 		// Write an error to STDERR, fallback display values
@@ -90,20 +85,16 @@ func main() {
 
 	// Check if speakers are muted. Set final
 	// output string accordingly.
-	if vol == -1 {
-		output = "<span foreground=\"#777777\"></span>  off"
-	} else if vol == 0 {
-		output = fmt.Sprintf(" %3d%%", vol)
-	} else if (vol > 0) && (vol <= 49) {
-		output = fmt.Sprintf(" %3d%%", vol)
-	} else {
-		output = fmt.Sprintf(" %3d%%", vol)
-	}
-
+	output = fmt.Sprintf("[%s] vol: %d%%", status, vol)
 	fullText = output
 	shortText = output
+	if status == "off" {
+		color = "#ffff00"
+	} else {
+		color = "#ffffff"
+	}
 
 	// Write out gathered information to STDOUT.
-	fmt.Fprintf(os.Stdout, "%s\n%s\n", fullText, shortText)
+	fmt.Fprintf(os.Stdout, "%s\n%s\n%s", fullText, shortText, color)
 	os.Exit(0)
 }
